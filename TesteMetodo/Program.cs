19fef6fb400892837255b92492d19f40d1eef6db
Usuario <Usuario@CPD02-M08>
2024-04-02 11:55:46 -0300

    :D

--- TesteMetodo/Program.cs
+++ TesteMetodo/Program.cs
@@ -10,11 +10,11 @@ namespace TesteMetodo // fiz isso só pra passar o tempo
     {
         static void MaiorCem(int numero) //isso aqui é um método; numero é um parametro dele; se eu chamar ele, ele executa o codigo!!
         {
-            if (numero > 100) 
+            if (numero > 100)
             {
                 Console.WriteLine("Número maior que 100!!!");
             }
-            else 
+            else
             {
                 Console.WriteLine("Número menor que 100 :(");
             }
@@ -25,10 +25,10 @@ namespace TesteMetodo // fiz isso só pra passar o tempo
         {
             int num = 0;
             Console.WriteLine("Digite um número!!!!!!");
-                num = int.Parse(Console.ReadLine());
+            num = int.Parse(Console.ReadLine());
 
             MaiorCem(num);//num usado como parametro pro metodo maior cem é chamado de argumento!
-            
+
             Console.ReadKey();
         }
     }
